a5897f80b4c847f35ab37bf4fe5491bc9ee37230
sagar rajak <sagarrajak245@gmail.com>
2024-05-31 15:40:12 +0530

    final steps

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ cloudinary.config({
 
 cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
 api_key:process.env.CLOUDINARY_API_KEY,
-api_secret:process.env.CLOUDINARY_API_SECRET, 
+api_secret:process.env.CLOUDINARY_API_SECRET,   
 
 });
 
@@ -45,7 +45,7 @@ app.use(express.json({ limit: "5mb" }));// to parse the incoming request with JS
 app.use(urlencoded({extended:true})); // to parse the incoming request with urlencoded payloads
  // to parse the incoming request with cookie payloads
  app.use(cookieParser());
-app.use("/api/auth",authRoutes);  
+app.use("/api/auth",authRoutes);    
 app.use("/api/users", userRoutes);         
 app.use('/api/posts', postRoutes); 
 app.use('/api/notifications', notificationRoutes);         
