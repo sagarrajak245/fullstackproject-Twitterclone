6e10d13ec36c71a965ae72d4af7493c564c37118
sagar rajak <sagarrajak245@gmail.com>
2024-06-01 18:39:41 +0530

    changes for heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,70 +1,55 @@
+
+
+
 // import { v2 as cloudinary } from 'cloudinary';
 // import cookieParser from 'cookie-parser';
 // import dotenv from 'dotenv';
 // import express, { urlencoded } from 'express';
-// import path from 'path';
+// import path, { dirname } from 'path';
+// import { fileURLToPath } from 'url';
 // import connectMongoDB from './db/connectmongodb.js';
 // import authRoutes from './routes/authroutes.js';
 // import notificationRoutes from './routes/notificationroutes.js';
 // import postRoutes from './routes/postroutes.js';
 // import userRoutes from './routes/userroutes.js';
 
-
-
-
 // dotenv.config();  
 
 // cloudinary.config({
-
-// cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-// api_key:process.env.CLOUDINARY_API_KEY,
-// api_secret:process.env.CLOUDINARY_API_SECRET,   
-
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,   
 // });
 
+// const app = express();     
 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
+// const port = process.env.PORT || 5001;              
 
+// app.use(express.json({ limit: "5mb" })); // to parse the incoming request with JSON payloads
+// app.use(urlencoded({ extended: true })); // to parse the incoming request with urlencoded payloads
+// app.use(cookieParser()); // to parse the incoming request with cookie payloads
 
-
-
-
-
-
-
-// const app =express();     
-
-// const __dirname = path.resolve();
-
-// const port=process.env.PORT || 5001;              
-   
-
-// app.use(express.json({ limit: "5mb" }));// to parse the incoming request with JSON payloads
-
-
-// app.use(urlencoded({extended:true})); // to parse the incoming request with urlencoded payloads
-//  // to parse the incoming request with cookie payloads
-//  app.use(cookieParser());
-// app.use("/api/auth",authRoutes);    
+// app.use("/api/auth", authRoutes);    
 // app.use("/api/users", userRoutes);         
 // app.use('/api/posts', postRoutes); 
 // app.use('/api/notifications', notificationRoutes);            
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static(path.join(__dirname,'/frontend/twitter-frontend/dist')));
-//     app.get('*',(req,res)=>{
-//         res.sendFile(path.resolve(__dirname,'frontend','twitter-frontend','dist','index.html'));    
-//     }
-//     );
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/twitter-frontend/dist')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../frontend/twitter-frontend/dist', 'index.html'));    
+//   });
 // }
 
 // console.log(process.env.MONGO_URI);     
 
-//  app.listen(port,()=>{
-//     console.log(`Server running on port ${port}`)   
-//     connectMongoDB();
-// }
-// ); 
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);   
+//   connectMongoDB();
+// });
 
 
 import { v2 as cloudinary } from 'cloudinary';
@@ -104,9 +89,9 @@ app.use('/api/posts', postRoutes);
 app.use('/api/notifications', notificationRoutes);            
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/twitter-frontend/dist')));
+  app.use(express.static(path.join(__dirname, 'public')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/twitter-frontend/dist', 'index.html'));    
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));    
   });
 }
 
