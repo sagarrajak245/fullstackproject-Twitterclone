e71adaada0383f1e262856b49cf717a1385096f7
sagar rajak <sagarrajak245@gmail.com>
2024-10-30 19:14:52 +0530

    khtam sab

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from 'cloudinary';
+
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express, { urlencoded } from 'express';
@@ -12,7 +12,11 @@ import userRoutes from './routes/userroutes.js';
 
 dotenv.config();  
 
-cloudinary.config({
+
+
+import * as cloudinary from 'cloudinary';
+
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,   
