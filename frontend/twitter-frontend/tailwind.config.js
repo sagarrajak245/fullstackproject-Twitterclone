f813be04e458bd713a783b3a928f607cfcc8f732
sagar rajak <sagarrajak245@gmail.com>
2025-06-16 00:12:43 +0530

    fix-9

--- frontend/twitter-frontend/tailwind.config.js
+++ frontend/twitter-frontend/tailwind.config.js
@@ -4,6 +4,10 @@ import daisyUIThemes from "daisyui/src/theming/themes";
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	darkMode: 'class',
+	safelist: [
+		"dark", "dark:bg-black", "dark:text-white",
+		"bg-black", "text-white", "data-theme=black"
+	],
 	theme: {
 		extend: {},
 	},
