a7ab2117fa8c5b6a0b531121306005d3f469919e
sagar rajak <sagarrajak245@gmail.com>
2025-06-16 00:27:06 +0530

    fix-10

--- frontend/twitter-frontend/tailwind.config.js
+++ frontend/twitter-frontend/tailwind.config.js
@@ -1,28 +1,48 @@
-import daisyui from "daisyui";
-import daisyUIThemes from "daisyui/src/theming/themes";
+import daisyui from 'daisyui';
+import daisyUIThemes from 'daisyui/src/theming/themes';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	darkMode: 'class',
+	content: [
+		'./index.html',
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
 	safelist: [
-		"dark", "dark:bg-black", "dark:text-white",
-		"bg-black", "text-white", "data-theme=black"
+		'dark',
+		'dark:bg-black',
+		'dark:text-white',
+		'bg-black',
+		'text-white',
+		'btn',
+		'bg-base-100',
+		'text-primary',
+		'theme-black',
+		'theme-forest',
+		'data-theme=black',
+		'data-theme=forest',
 	],
 	theme: {
 		extend: {},
 	},
 	plugins: [daisyui],
-
 	daisyui: {
+		styled: true,
 		themes: [
-			"light",
+			'light', // base theme
+			'forest', // optional test theme (to confirm DaisyUI works in prod)
 			{
 				black: {
-					...daisyUIThemes["black"],
-					primary: "rgb(29, 155, 240)",
-					secondary: "rgb(24, 24, 24)",
+					...daisyUIThemes['black'],
+					primary: 'rgb(29, 155, 240)',
+					secondary: 'rgb(24, 24, 24)',
 				},
 			},
 		],
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: '',
 	},
 };
